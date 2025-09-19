fd7dd9d846ee9a80a94a81a3253ad838789cb325
TuanTran0168 <tamthy12345@gmail.com>
2025-09-19 14:19:47 +0700

    [ADD][config/cors-ip-config] remove hardcode host in swagger

--- book-management-backend/docs/docs.go
+++ book-management-backend/docs/docs.go
@@ -346,7 +346,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "",
 	BasePath:         "/api",
 	Schemes:          []string{},
 	Title:            "Book Management API",
